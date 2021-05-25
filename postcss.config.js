cc2bdc30f1fbcc96cab2a2f463cc9afea0e27c07
Tim McKay <timbomckay@gmail.com>
2021-05-25 00:59:18 -0400

    purgecss safelisting focus-visible

--- postcss.config.js
+++ postcss.config.js
@@ -4,16 +4,16 @@ const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [ './hugo_stats.json' ],
   defaultExtractor: (content) => {
-      let els = JSON.parse(content).htmlElements;
-      return els.tags.concat(els.classes, els.ids);
+    let els = JSON.parse(content).htmlElements;
+    return els.tags.concat(els.classes, els.ids);
   }
 });
 
 module.exports = {
   plugins: [
     require('postcss-import')({ path: [themeDir + '/assets/'] }),
-    require('tailwindcss')(themeDir + '/tailwind.config.js'),
     require('postcss-nesting'),
+    require('tailwindcss')(themeDir + '/tailwind.config.js'),
     ...process.env.HUGO_ENVIRONMENT === 'production'
       ? [autoprefixer, purgecss]
       : []
